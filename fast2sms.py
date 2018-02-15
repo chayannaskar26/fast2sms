4d4674fc138eb2b22e03fdb0b3381264d5032660
CHAYAN NASKAR <chayannaskar@live.com>
2018-02-15 22:06:09 +0530

    First commit

---
+++ fast2sms.py
@@ -0,0 +1,22 @@
+# fast2sms
+# API URI http://api.fast2sms.com/sms.php?token=&mob=&mess=&sender=FSTSMS&route=0
+# By Chayan Naskar
+
+import urllib2
+import json
+
+def sendSms(token):
+	number = raw_input("Enter mobile number : ")
+	msg = raw_input("Enter the message : ")
+	url = "http://api.fast2sms.com/sms.php?token=" + token + "&mob=" + number + "&mess=" + msg + "&sender=FSTSMS&route=0"
+	send_sms = urllib2.urlopen(url).read()
+	jsonData = json.loads(send_sms)
+	if(jsonData["status"] == "accept"):
+		print "Last SMS Cost ", jsonData["cost"]
+		print "Remaining Balance ", jsonData["balance"]
+	else:
+		print "Error Sending SMS : ", jsonData['error_message']
+
+
+token = raw_input("Please enter Token : ")
+sendSms(token)
